b00b1de10bfe1f9afd5b160be8254ea0d1b5724d
rajib-2023-29 <rajibmondal2005r@gmail.com>
2024-03-18 19:39:38 +0530

    area of square

--- codechef/course/LBCL01_problems_CLB005.c
+++ codechef/course/LBCL01_problems_CLB005.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
+//area of square
 int main(){
-printf ("hello world");
+    int side;
+    printf ("enter side");
+    scanf ("%d",&side);
+    int area=side*side;
+printf ("area is;%d",area);
 return 0;
 }
