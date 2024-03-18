4ff7bc24d3d5cdf9dabf94b9b740fec331d4249d
rajib-2023-29 <rajibmondal2005r@gmail.com>
2024-03-18 19:28:47 +0530

    just hello world

--- codechef/course/LBCL01_problems_CLB005.c
+++ codechef/course/LBCL01_problems_CLB005.c
@@ -1,9 +1,5 @@
-//Try to add 21 and 40 in code and print the result. Remember, we don't need "" (double quotes) when printing numbers.
-
-#include <stdio.h>
-
-int main() {
-  int a = 21 + 40;
-  printf ("%d",a);
-  return 0;
+#include<stdio.h>
+int main(){
+printf ("hello world");
+return 0;
 }
